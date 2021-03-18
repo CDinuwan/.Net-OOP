97a9943f186961e14cbddc81220f2bd96e07ddbf
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-18 19:34:07 +0530

    Basics

---
+++ OOP .Net/OOP .Net/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_.Net
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public int Energy { get; set; }
+
+        public Person(int age,int energy)
+        {
+            this.Age = age;
+            this.Energy = energy;
+        }
+        public void TellLifeStory()
+        {
+            Console.WriteLine("My age is "+Age+" and my energy is "+Energy);
+        }
+    }
+}
---
+++ OOP .Net/OOP .Net/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP_.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chanuka = new Person(24, 100);
+            var dilini = new Person(21, 90);
+            var tharun = new Person(15, 80);
+
+            chanuka.TellLifeStory();
+            dilini.TellLifeStory();
+            tharun.TellLifeStory();
+        }
+
+    }
+}
